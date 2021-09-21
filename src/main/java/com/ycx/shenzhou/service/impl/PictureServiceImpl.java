ea8e61f308d2b67d57354ccb37606359007a7676
iamcht <605908700@qq.com>
2021-09-21 09:42:19 +0800

    写了字母

--- src/main/java/com/ycx/shenzhou/service/impl/PictureServiceImpl.java
+++ src/main/java/com/ycx/shenzhou/service/impl/PictureServiceImpl.java
@@ -19,7 +19,7 @@ public class PictureServiceImpl implements PictureService {
     private static final String RESULT_URL = "file/img/";
     private static final String DEFAULT_FOLDER = "file/default/";
     private static final int RANDOM_FILE_NAME_LENGTH = 15;
-    private static final char[] BASE_CHARACTER = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b'};
+    private static final char[] BASE_CHARACTER = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
     @Autowired
     private PictureMapper pictureMapper;
